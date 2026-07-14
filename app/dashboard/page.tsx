cae320596d5599fc2a2628b6b9c1f0315a9bf16d
barberbench-iq <barberbenchiq@gmail.com>
2026-07-14 22:26:58 +1200

    Delete app directory

--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-import { DashboardClient } from "@/components/DashboardClient";
-import { Sidebar } from "@/components/Sidebar";
-export default function DashboardPage(){return <main className="dashboardShell"><Sidebar/><DashboardClient/></main>}
